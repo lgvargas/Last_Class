50f13f9c4efe00c7b7459fb9b62556a2081770be
lgvargas <leticia.vargas@wizeline.com>
2019-05-04 14:30:04 -0500

    nuevo intento

--- index.js
+++ index.js
@@ -1,2 +1,1 @@
 require('./server');
-console.log('Hola');
--- server/index.js
+++ server/index.js
@@ -13,13 +13,12 @@ app.use(bodyParser.json());
 const gatosRoutes = require('./routes/gatosRoutes');
 gatosRoutes(app);
 
-if (process.env.NODE_ENV === 'production')
-{
-	app.use(express.static('client/build'));
-	const path = require('path');
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
-	});
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    const path = require('path');
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
+    })
 }
 
 app.listen(process.env.PORT || 5000);
